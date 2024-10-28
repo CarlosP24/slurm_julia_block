1702412d3db759a5263c8881862d6ae2d22a52cc
Carlos Payá <carlos.paya@csic.es>
2024-10-28 17:24:42 +0100

    for esbirro

--- launch_slurm.jl
+++ launch_slurm.jl
@@ -17,7 +17,7 @@ addprocs(max(0, maxprocs + 1 - nworkers()))
 # end 
 
 ## Run code
-include(@__DIR__*"src/main.jl")
+include(joinpath(@__DIR__, "src/main.jl"))
 
 ## Clean up
 rmprocs(workers()...)
