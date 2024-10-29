d370010b0c831014dff1408cd7518e52589799d3
Carlos Payá <carlos.paya@csic.es>
2024-10-29 11:05:49 +0100

    test paths

--- launch_esbirro.jl
+++ launch_esbirro.jl
@@ -6,7 +6,14 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --output="slurm.out/%j.out"
 
-script_path = read(`scontrol show job $ENV["SLURM_JOBID"] | awk -F'Command=' '{print $2}'`, String)
+# Construct the scontrol command
+scontrol_cmd = `scontrol show job $(ENV["SLURM_JOBID"])`
+
+# Construct the awk command separately
+awk_cmd = `awk -F'Command=' '{print $2}'`
+
+# Use pipeline to pass the output of scontrol_cmd to awk_cmd
+script_path = read(pipeline(scontrol_cmd, awk_cmd), String)
 
 ## Julia setup
 using Distributed
