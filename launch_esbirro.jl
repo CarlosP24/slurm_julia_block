16ab3c3147a194a5e5e71f6a240252b00992006d
Carlos Payá <carlos.paya@csic.es>
2024-10-29 17:14:47 +0100

    test

--- launch_esbirro.jl
+++ launch_esbirro.jl
@@ -29,7 +29,28 @@ script_path = ENV["SLURM_SUBMIT_DIR"]
 # end
 
 ## Run code
-include("$(script_path)/src/main.jl")
+# include("$(script_path)/src/main.jl")
+
+function mwe()
+    lat = LP.honeycomb();
+    model= @hopping((; t = 2.7) -> t*I);
+    h = lat |> model
+
+    g = h |> greenfunction()
+
+    trng = range(0, 5, length = 100)
+    ωrng = range(-1, 1, length = 100) .+ 1e-3im
+
+    pts = Iterators.product(ωrng, trng)
+    LDOS = @showprogress pmap(pts) do pt 
+        ω, t = pt
+        return ldos(g[cells = (1, 1)])(ω; t)
+    end
+    return LDOS
+end
+
+LDOS = mwe()
+save("LDOS.jld2", "LDOS", LDOS)
 
 ## Clean up
 rmprocs(workers()...)
