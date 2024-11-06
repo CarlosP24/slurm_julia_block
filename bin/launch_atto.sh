c6eaae2001c4dfd78d3d5b54a6150d0cfa727663
Carlos Payá <carlos.paya@csic.es>
2024-11-06 16:12:18 +0100

    add support for slurm arrays

--- bin/launch_atto.sh
+++ bin/launch_atto.sh
@@ -3,15 +3,45 @@ source config/prologue.sh
 if [ $? -ne 0 ]; then
   exit 1
 fi
-sbatch <<EOT
+# Define parameters array
+if [ -f "$1" ]; then
+  PARAMS=($(cat "$1"))
+else
+  PARAMS=("$@")
+fi
+ARRAY_SIZE=${#PARAMS[@]}
+
+if [ $ARRAY_SIZE -eq 0 ]; then
+  echo "No parameters provided"
+  exit 1
+fi
+
+# Serialize the array into a string
+PARAMS_STR=$(IFS=,; echo "${PARAMS[*]}")
+export PARAMS_STR
+
+# Launch the job
+sbatch --export=ALL <<EOT
 #!/bin/bash
 ## Slurm header
 #SBATCH --partition=most
-#SBATCH --ntasks=384
+#SBATCH --ntasks-per-node=196
+#SBATCH --nodes=2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
-#SBATCH --output="logs/%j.out"
-#SBATCH --job-name="${PWD##*/}"
+#SBATCH --output="logs/%A_%a.out"
+#SBATCH --job-name="${PWD##*/}_$ARRAY_SIZE"
+#SBATCH --mail-user=carlos.paya@csic.es
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
+#SBATCH --array=1-$ARRAY_SIZE
 
-julia --project bin/launcher.jl "$@"
+# Deserialize
+IFS=, read -r -a PARAMS <<< "\$PARAMS_STR"
+
+# Select the parameter
+PARAM="\${PARAMS[\$SLURM_ARRAY_TASK_ID-1]}"
+echo "Running \$PARAM"
+
+# Run the job
+julia --project bin/launcher.jl "\$PARAM"
 EOT
--- bin/launch_esbirro.sh
+++ bin/launch_esbirro.sh
@@ -3,15 +3,46 @@ source config/prologue.sh
 if [ $? -ne 0 ]; then
   exit 1
 fi
-sbatch <<EOT
+# Define parameters array
+if [ -f "$1" ]; then
+  PARAMS=($(cat "$1"))
+else
+  PARAMS=("$@")
+fi
+ARRAY_SIZE=${#PARAMS[@]}
+
+if [ $ARRAY_SIZE -eq 0 ]; then
+  echo "No parameters provided"
+  exit 1
+fi
+
+# Serialize the array into a string
+PARAMS_STR=$(IFS=,; echo "${PARAMS[*]}")
+export PARAMS_STR
+
+# Launch the job
+sbatch --export=ALL <<EOT
 #!/bin/bash
 ## Slurm header
 #SBATCH --partition=esbirro
-#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=2
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
-#SBATCH --output="logs/%j.out"
-#SBATCH --job-name="${PWD##*/}"
+##SBATCH --mem-per-cpu=2G
+#SBATCH --output="logs/%A_%a.out"
+#SBATCH --job-name="${PWD##*/}_$ARRAY_SIZE"
+#SBATCH --mail-user=carlos.paya@csic.es
+#SBATCH --mail-type=END,FAIL
+#SBATCH --array=1-$ARRAY_SIZE
 
-julia --project bin/launcher.jl "$@"
+# Deserialize
+IFS=, read -r -a PARAMS <<< "\$PARAMS_STR"
+
+# Select the parameter
+PARAM="\${PARAMS[\$SLURM_ARRAY_TASK_ID-1]}"
+echo "Running \$PARAM"
+
+# Run the job
+julia --project bin/launcher.jl "\$PARAM"
+
 EOT
