e37da39b2c9293a3e413660c77279116ee443566
Carlos Payá <carlos.paya@csic.es>
2024-12-05 15:26:31 +0100

    update prologue to fix julia version

--- config/prologue.sh
+++ config/prologue.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Ensure juliaup is installed and use the specific version of Julia
+if ! command -v juliaup &> /dev/null; then
+    echo "juliaup could not be found. Please install juliaup."
+    exit 1
+fi
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 depot_path="$script_dir/../.julia_depot"
 mkdir -p "$depot_path"
@@ -6,6 +12,13 @@ mkdir -p "logs"
 mkdir -p "data"
 export JULIA_DEPOT_PATH="$depot_path"
 export JULIA_PROJECT="$script_dir/.."
+
+export JULIAUP_CHANNEL=1.11.1
+
+if ! juliaup status | grep " $JULIAUP_CHANNEL "; then
+  echo "Julia version $JULIAUP_CHANNEL not installed. Installing now..."
+  juliaup install "$JULIAUP_CHANNEL"
+fi 
 
 julia --project "$script_dir/prologue.jl"
 
